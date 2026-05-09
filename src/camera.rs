84d110c1a000f2a1e6bb4a6cac9bc47ac24eb4c7
Alexi Chepura <a.chepura@gmail.com>
2026-05-09 22:32:52 +0300

    camera

--- src/camera.rs
+++ src/camera.rs
@@ -16,7 +16,7 @@ impl Plugin for CameraPlugin {
 fn spawn_camera(mut commands: Commands) {
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(1.0, 0.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_xyz(0.3, 0.2, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
         FreeCamera {
             sensitivity: 0.2,
             friction: 25.0,
